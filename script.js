2e310d79f989003b123e4596e38130085c6da7f2
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-12 15:03:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+
+const inputMin = document.querySelector(`.min`);
+const inputMax = document.querySelector(`.max`);
+const buttonNewNumber = document.querySelector(`.new-number`);
+const displayRandomNumber = document.querySelector(`.display-number`);
+
